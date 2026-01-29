ebe8da7782c3d05d4b0684c573495f6785faec11
DJisaiah <dariesisaiah@gmail.com>
2026-01-29 21:05:13 +0200

    day 36

---
+++ CodeWars/8Kyu/C/Square(n) Sum/solution.c
@@ -0,0 +1,9 @@
+#include <stddef.h>
+
+int square_sum(const int values[/* count */], size_t count) {
+  int sum = 0;
+  for (size_t i = 0; i < count; i++) {
+    sum += values[i] * values[i];
+  }
+  return sum;
+}
